7f84c680adcbb63bb3e7b509d22f3f08b678f5b5
Cedric Abuso <cedricabuso@gmail.com>
2016-04-20 17:04:49 +0800

    Updates

--- src/Torann/GeoIP/GeoIP.php
+++ src/Torann/GeoIP/GeoIP.php
@@ -7,12 +7,12 @@ use Illuminate\Session\Store as SessionStore;
 
 class GeoIP {
 
-	/**
-	 * The session store.
-	 *
-	 * @var \Illuminate\Session\Store
-	 */
-	protected $session;
+    /**
+     * The session store.
+     *
+     * @var \Illuminate\Session\Store
+     */
+    protected $session;
 
     /**
      * Illuminate config repository instance.
@@ -21,167 +21,169 @@ class GeoIP {
      */
     protected $config;
 
-	/**
-	 * Remote Machine IP address.
-	 *
-	 * @var float
-	 */
-	protected $remote_ip = null;
+    /**
+     * Remote Machine IP address.
+     *
+     * @var float
+     */
+    protected $remote_ip = null;
 
-	/**
-	 * Location data.
-	 *
-	 * @var array
-	 */
-	protected $location = null;
+    /**
+     * Location data.
+     *
+     * @var array
+     */
+    protected $location = null;
 
-	/**
-	 * Reserved IP address.
-	 *
-	 * @var array
-	 */
-	protected $reserved_ips = array (
-		array('0.0.0.0','2.255.255.255'),
-		array('10.0.0.0','10.255.255.255'),
-		array('127.0.0.0','127.255.255.255'),
-		array('169.254.0.0','169.254.255.255'),
-		array('172.16.0.0','172.31.255.255'),
-		array('192.0.2.0','192.0.2.255'),
-		array('192.168.0.0','192.168.255.255'),
-		array('255.255.255.0','255.255.255.255')
-	);
+    /**
+     * Reserved IP address.
+     *
+     * @var array
+     */
+    protected $reserved_ips = array (
+        array('0.0.0.0','2.255.255.255'),
+        array('10.0.0.0','10.255.255.255'),
+        array('127.0.0.0','127.255.255.255'),
+        array('169.254.0.0','169.254.255.255'),
+        array('172.16.0.0','172.31.255.255'),
+        array('192.0.2.0','192.0.2.255'),
+        array('192.168.0.0','192.168.255.255'),
+        array('255.255.255.0','255.255.255.255')
+    );
 
-	/**
-	 * Default Location data.
-	 *
-	 * @var array
-	 */
-	protected $default_location = array (
-		"ip" 			=> "127.0.0.0",
-		"isoCode" 		=> "US",
-		"country" 		=> "United States",
-		"city" 			=> "New Haven",
-		"state" 		=> "CT",
-		"postal_code" 	=> "06510",
-		"lat" 			=> 41.31,
-		"lon" 			=> -72.92
-	);
+    /**
+     * Default Location data.
+     *
+     * @var array
+     */
+    protected $default_location = array (
+        "ip"            => "127.0.0.0",
+        "isoCode"       => "US",
+        "country"       => "United States",
+        "city"          => "New Haven",
+        "state"         => "CT",
+        "postal_code"   => "06510",
+        "lat"           => 41.31,
+        "lon"           => -72.92,
+        "default"       => true
+    );
 
-	/**
-	 * Create a new GeoIP instance.
-	 *
+    /**
+     * Create a new GeoIP instance.
+     *
      * @param  \Illuminate\Config\Repository  $config
-	 * @param  \Illuminate\Session\Store  $session
-	 * @param  \Symfony\Component\HttpFoundation\Request   $request
-	 * @return void
-	 */
-	public function __construct(Repository $config, SessionStore $session, Request $request)
-	{
-		$this->config = $config;
-		$this->session = $session;
+     * @param  \Illuminate\Session\Store  $session
+     * @param  \Symfony\Component\HttpFoundation\Request   $request
+     * @return void
+     */
+    public function __construct(Repository $config, SessionStore $session, Request $request)
+    {
+        $this->config = $config;
+        $this->session = $session;
 
-		$this->remote_ip = $this->default_location['ip'] = $request->getClientIp();
-	}
+        $this->remote_ip = $this->default_location['ip'] = $request->getClientIp();
+    }
 
-	/**
-	 * Save location data in the session.
-	 *
-	 * @return void
-	 */
-	function saveLocation() {
-		$this->session->set('geoip-location', $this->location);
-	}
+    /**
+     * Save location data in the session.
+     *
+     * @return void
+     */
+    function saveLocation() {
+        $this->session->set('geoip-location', $this->location);
+    }
 
-	/**
-	 * Get location from IP.
-	 *
-	 * @param  string $ip Optional
-	 * @return array
-	 */
-	function getLocation( $ip = null ) {
-		// Get location data
-		$this->location = $this->find( $ip );
+    /**
+     * Get location from IP.
+     *
+     * @param  string $ip Optional
+     * @return array
+     */
+    function getLocation( $ip = null ) {
+        // Get location data
+        $this->location = $this->find( $ip );
 
-		// Save user's location
-		if( $ip === null ) {
-			$this->saveLocation();
-		}
+        // Save user's location
+        if( $ip === null ) {
+            $this->saveLocation();
+        }
 
-		return $this->location;
-	}
+        return $this->location;
+    }
 
-	private function find( $ip = null ) {
-		// Check Session
-		if ( $ip === null && $position = $this->session->get('geoip-location') ) {
-			if($position['ip'] === $this->remote_ip) {
-				return $position;
-			}
-		}
+    private function find( $ip = null ) {
+        // Check Session
+        if ( $ip === null && $position = $this->session->get('geoip-location') ) {
+            if($position['ip'] === $this->remote_ip) {
+                return $position;
+            }
+        }
 
-		// If IP not set, user remote IP
-		if ( $ip === null ) {
-			$ip = $this->remote_ip;
-		}
+        // If IP not set, user remote IP
+        if ( $ip === null ) {
+            $ip = $this->remote_ip;
+        }
 
-		// Check if the ip is not local or empty
-		if( $this->checkIp( $ip ) ) {
+        // Check if the ip is not local or empty
+        if( $this->checkIp( $ip ) ) {
 
-			// Call default service
-			$service = 'locate_'.$this->config->get('geoip::service');
-			return $this->$service($ip);
-		}
+            // Call default service
+            $service = 'locate_'.$this->config->get('geoip::service');
+            return $this->$service($ip);
+        }
 
-		return $this->default_location;
-	}
+        return $this->default_location;
+    }
 
-	// Maxmind
-	private function locate_maxmind( $ip ) {
+    // Maxmind
+    private function locate_maxmind( $ip ) {
 
-		$settings = $this->config->get('geoip::maxmind');
+        $settings = $this->config->get('geoip::maxmind');
 
-		if($settings['type'] === 'web_service') {
-			$maxmind = new Client($settings['user_id'], $settings['license_key']);
-		}
-		else {
-			$maxmind = new Reader(app_path().'/database/maxmind/GeoLite2-City.mmdb');
-		}
+        if($settings['type'] === 'web_service') {
+            $maxmind = new Client($settings['user_id'], $settings['license_key']);
+        }
+        else {
+            $maxmind = new Reader(app_path().'/database/maxmind/GeoLite2-City.mmdb');
+        }
 
-		$record = $maxmind->city($ip);
+        $record = $maxmind->city($ip);
 
-		$location = array(
-			"ip"			=> $ip,
-			"isoCode" 		=> $record->country->isoCode,
-			"country" 		=> $record->country->name,
-			"city" 			=> $record->city->name,
-			"state" 		=> $record->mostSpecificSubdivision->isoCode,
-			"postal_code" 	=> $record->postal->code,
-			"lat" 			=> $record->location->latitude,
-			"lon" 			=> $record->location->longitude
-		);
+        $location = array(
+            "ip"            => $ip,
+            "isoCode"       => $record->country->isoCode,
+            "country"       => $record->country->name,
+            "city"          => $record->city->name,
+            "state"         => $record->mostSpecificSubdivision->isoCode,
+            "postal_code"   => $record->postal->code,
+            "lat"           => $record->location->latitude,
+            "lon"           => $record->location->longitude,
+            "default"       => true
+        );
 
-		unset($record);
+        unset($record);
 
-		return $location;
-	}
+        return $location;
+    }
 
-	//Checks if the ip is not local or empty
-	private function checkIp( $ip ) {
-		$longip = ip2long($ip);
+    //Checks if the ip is not local or empty
+    private function checkIp( $ip ) {
+        $longip = ip2long($ip);
 
-		if ( !empty($ip) ) {
+        if ( !empty($ip) ) {
 
-			foreach ($this->reserved_ips as $r) {
-				$min = ip2long($r[0]);
-				$max = ip2long($r[1]);
+            foreach ($this->reserved_ips as $r) {
+                $min = ip2long($r[0]);
+                $max = ip2long($r[1]);
 
-				if ($longip >= $min && $longip <= $max) {
-					return false;
-				}
-			}
-			return true;
-		}
+                if ($longip >= $min && $longip <= $max) {
+                    return false;
+                }
+            }
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
 }
